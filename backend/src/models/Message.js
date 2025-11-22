2d154f0b6ba6d07b71dff602b8823aea2120eb55
ndt-dinhtu <ndt.290303060703>
2025-11-22 10:04:52 +0700

    tạo các model

---
+++ backend/src/models/Message.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    conversationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Conversation",
+      required: true,      
+      index: true,
+    },
+    senderId: {            
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      index: true,
+    },
+    content: {
+      type: String,
+      trim: true,
+    },
+    imgUrl: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+messageSchema.index({ conversationId: 1, createdAt: -1 });
+
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
